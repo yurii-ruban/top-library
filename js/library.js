634d5db0c56265e4d44fcc3d263cefed2b3cc691
Yurii Ruban <yurii.ruban97@gmail.com>
2025-03-12 16:02:09 +0000

    feat: add adding books feature
    TODO:
     - Add read/not read changes
     - Add logic for removing the book

--- js/library.js
+++ js/library.js
@@ -1,7 +1,9 @@
 const myLibrary = [];
+const readSrc = "./img/read.svg";
+const notReadSrc = "./img/not_read.svg";
 
 function Entity() {
-    this.id = self.crypto.randomUUID();
+  this.id = self.crypto.randomUUID();
 };
 
 Entity.prototype.getId = function() {
@@ -9,6 +11,7 @@ Entity.prototype.getId = function() {
 }
 
 function Book(title, author, pages, read) {
+    this.id = self.crypto.randomUUID();
     this.title = title;
     this.author = author;
     this.pages = pages;
@@ -17,8 +20,83 @@ function Book(title, author, pages, read) {
 
 Object.setPrototypeOf(Book.prototype, Entity.prototype);
 
-function addBookToLibrary() {
-  // take params, create a book then store it in the array
+function createCard () {
+  const cardContainer = document.createElement("div");
+  cardContainer.classList.add("card");
+
+  const infoContainer = document.createElement("div");
+  infoContainer.classList.add("info");
+  const cardHeader = document.createElement("h3");
+  cardHeader.classList.add("title");
+  const cardAuthor = document.createElement("p");
+  cardAuthor.classList.add("author");
+  const cardPages = document.createElement("p");
+  cardPages.classList.add("pages");
+  const cardId = document.createElement("p");
+  cardId.classList.add("id");
+  infoContainer.appendChild(cardHeader);
+  infoContainer.appendChild(cardAuthor);
+  infoContainer.appendChild(cardPages);
+  infoContainer.appendChild(cardId);
+
+  const controlsContainer = document.createElement("div");
+  controlsContainer.classList.add("controls");
+  const readBtn = document.createElement("button");
+  readBtn.classList.add("haveread");
+  const readImg = document.createElement("img");
+  readImg.width = "24";
+  readImg.height = "24";
+  readBtn.appendChild(readImg);
+  const removeBtn = document.createElement("button");
+  removeBtn.classList.add("remove-book");
+  controlsContainer.appendChild(readBtn);
+  controlsContainer.appendChild(removeBtn);
+
+  cardContainer.appendChild(infoContainer);
+  cardContainer.appendChild(controlsContainer);
+
+  return {
+    cardHeader,
+    cardAuthor,
+    cardPages,
+    cardId,
+    readBtn,
+    readImg,
+    removeBtn,
+    cardContainer
+  };
+
+}
+
+function fillCardInfo(cardObj) {
+  const lastBook = myLibrary[myLibrary.length - 1];
+  cardObj.cardHeader.textContent = lastBook.title;
+  cardObj.cardAuthor.textContent = "Author: " + lastBook.author;
+  cardObj.cardPages.textContent = "Pages: " + lastBook.pages;
+  cardObj.cardId.textContent = "Id: " + lastBook.getId();
+  cardObj.readImg.src = lastBook.read ? readSrc : notReadSrc;
+  cardObj.removeBtn.textContent = "Remove this book";
+}
+
+
+const libraryContainer = document.querySelector(".cards-container");
+const addBtn = document.querySelector(".submit");
+addBtn.addEventListener('click', addBookToLibrary, false);
+
+
+function addBookToLibrary(clickEv) {
+  clickEv.preventDefault()
+  const bookName = document.querySelector('#title').value;
+  const bookAuthor = document.querySelector('#author').value;
+  const bookPages = document.querySelector('#pages').value;
+  const bookRead = Boolean(document.querySelector('#read').checked);
+
+  myLibrary.push(new Book(bookName, bookAuthor, bookPages, bookRead));
+
+  const cardMockup = createCard();
+  fillCardInfo(cardMockup);
+
+  libraryContainer.appendChild(cardMockup.cardContainer);
 }
 
 function removeBookFromLibrary(id) {
@@ -26,8 +104,6 @@ function removeBookFromLibrary(id) {
 }
 
 const resizeBtn = document.querySelector(".sidebar-expander");
-
-
 resizeBtn.addEventListener("click", function (e) {
   document.body.classList.toggle("sb-expanded");
 });
